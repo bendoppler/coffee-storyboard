8766de7f1ce52d2dadd0e94cd4bcbf44a771ed77
piendop <baodo789@gmail.com>
2019-11-12 19:06:10 +0700

    finish facebook login segue to new screen

--- coffee-storyboard/LoginViewController.swift
+++ coffee-storyboard/LoginViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 import GoogleSignIn
 import FacebookLogin
 import CoreData
+import FacebookCore
 
-class LoginViewController: UIViewController, GIDSignInDelegate {
-    
+class LoginViewController: UIViewController, GIDSignInDelegate, LoginButtonDelegate {
     let buttonRatio: CGFloat = 5/24
     var fbSigninButton: FBLoginButton!
     var ggSiginButton: GIDSignInButton!
@@ -29,7 +29,7 @@ class LoginViewController: UIViewController, GIDSignInDelegate {
         GIDSignIn.sharedInstance()?.presentingViewController = self
         let fbPosition = CGPoint(x: view.center.x-buttonWidth/2+3, y: view.center.y - buttonHeight + magicNumber - 8)
         let ggPosition = CGPoint(x: view.center.x-buttonWidth/2, y: view.center.y)
-        self.fbSigninButton = FBLoginButton(frame: CGRect(origin: fbPosition, size: CGSize(width: buttonWidth-5, height: buttonHeight-magicNumber)), permissions: [.publicProfile])
+        self.fbSigninButton = FBLoginButton(frame: CGRect(origin: fbPosition, size: CGSize(width: buttonWidth-5, height: buttonHeight-magicNumber)), permissions: [.publicProfile, .email])
         self.ggSiginButton = GIDSignInButton(frame: CGRect(origin: ggPosition, size: CGSize(width: buttonWidth, height: buttonHeight)))
         let fbLayoutConstraints = self.fbSigninButton.constraints
         for lc in fbLayoutConstraints {
@@ -38,8 +38,44 @@ class LoginViewController: UIViewController, GIDSignInDelegate {
                 break;
             }
         }
+        self.fbSigninButton.delegate = self
         view.addSubview(self.fbSigninButton)
         view.addSubview(self.ggSiginButton)
+    }
+    
+    //MARK: Facebook sigin
+    func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
+        if let error = error {
+            if(error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
+                print("The user has not signed in before or they have since signed out")
+            } else {
+                print("\(error.localizedDescription)")
+            }
+            return
+        }
+        if let accessToken = result?.token{
+            let graphPath = "/me"
+            let parameters: [String: Any] = ["fields": "id,first_name,last_name,email"]
+            let httpMethod: HTTPMethod = .get
+            
+            let request = GraphRequest(graphPath: graphPath, parameters: parameters, tokenString: accessToken.tokenString, version: "v5.0", httpMethod: httpMethod)
+            request.start { [weak self] response, result, error in
+                if error != nil {
+                    print("Cannot get user info from user token")
+                }
+                if let result = result as? [String:String] {
+                    self?.container?.performBackgroundTask{ context in
+                        try? User.saveUserInfo(userId: result["id"] ?? accessToken.userID, familyName: result["first_name"] ?? "", name: result["last_name"] ?? "", birthday: nil, phoneNumber: nil, email: result["email"] ?? "", in: context)
+                        try? context.save()
+                    }
+                    self?.peformSegueToIntroScreen()
+                }
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+        // Perform any operations when the user disconnects from app here.
     }
     
     //MARK: Google sign in
@@ -53,18 +89,20 @@ class LoginViewController: UIViewController, GIDSignInDelegate {
             return
         }
         container?.performBackgroundTask{ context in
-            // Perform any operations on signed in user here.
             try? User.saveUserInfo(userId: user.userID, familyName: user.profile.familyName, name: user.profile.name, birthday: nil, phoneNumber: nil, email: user.profile.email, in: context)
             try? context.save()
         }
-        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-        let introVC = storyBoard.instantiateViewController(withIdentifier: "IntroMVC")
-        self.present(introVC, animated: true, completion: nil)
+        peformSegueToIntroScreen()
     }
     
     func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
         // Perform any operations when the user disconnects from app here.
     }
 
+    private func peformSegueToIntroScreen() {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let introVC = storyBoard.instantiateViewController(withIdentifier: "IntroMVC")
+        self.present(introVC, animated: true, completion: nil)
+    }
 }
 
