3d67cc608c90073b8f14ab48679a66c697e9259c
piendop <baodo789@gmail.com>
2019-11-20 15:33:46 +0700

    disable battery time aka status bar

--- coffee-storyboard/HomeViewController.swift
+++ coffee-storyboard/HomeViewController.swift
@@ -9,5 +9,18 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        if let navController = navigationController {
+            System.clearNavigationBar(forBar: navController.navigationBar)
+            navController.view.backgroundColor = .clear
+        }
+    }
+}
 
+struct System {
+    static func clearNavigationBar(forBar navBar: UINavigationBar) {
+        navBar.setBackgroundImage(UIImage(), for: .default)
+        navBar.shadowImage = UIImage()
+        navBar.isTranslucent = true
+    }
 }
